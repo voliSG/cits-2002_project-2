027c3bad1ca37fb654b42e1169470bbedd4c2821
agniG <88413285+agniG@users.noreply.github.com>
2021-10-14 12:23:32 +0800

    dfs

--- duplicates.c
+++ duplicates.c
@@ -1,6 +1,23 @@
 #include <duplicates.h>
-
+#define OPTLIST   "aAfhlq"
 int main(int argc, char *argv[])
 {
-  
+  int opt;
+
+  while((opt = getopt(argc, argv, OPTLIST)) != -1){
+    switch (opt) {
+      case 'a':
+
+      case 'A':
+        exit(EXIT_FAILURE);
+      case 'f':
+
+      case 'h':
+
+      case 'l':
+
+      case 'q':
+      
+    }
+  }
 }
