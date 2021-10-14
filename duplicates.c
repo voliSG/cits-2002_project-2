de4c3898b240740103285abe576571cce4cc10d8
Sean <2003seangarcia@gmail.com>
2021-10-14 12:56:19 +0800

    printf test

--- duplicates.c
+++ duplicates.c
@@ -1,4 +1,6 @@
 #include "duplicates.h"
+
+
 #define OPTLIST   "aAfhlq"
 
 int main(int argc, char *argv[])
@@ -8,16 +10,22 @@ int main(int argc, char *argv[])
   while((opt = getopt(argc, argv, OPTLIST)) != -1){
     switch (opt) {
       case 'a':
+        printf("a\n");
         break;
       case 'A':
+        printf("A\n");
         exit(EXIT_FAILURE);
       case 'f':
+        printf("f\n");
         break;
       case 'h':
+        printf("h\n");
         break;
       case 'l':
+        printf("l\n");
         break;
       case 'q':
+        printf("q\n");
         break;
     }
   }
