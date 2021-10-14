dd75d5b2ff29a9ffd9728fba1379fc0e08ba3398
agniG <88413285+agniG@users.noreply.github.com>
2021-10-14 12:50:28 +0800

    ;(

--- duplicates.c
+++ duplicates.c
@@ -1,4 +1,4 @@
-#include "duplicates.h"
+#include <duplicates.h>
 #define OPTLIST   "aAfhlq"
 int main(int argc, char *argv[])
 {
@@ -7,18 +7,16 @@ int main(int argc, char *argv[])
   while((opt = getopt(argc, argv, OPTLIST)) != -1){
     switch (opt) {
       case 'a':
-        break;
 
       case 'A':
         exit(EXIT_FAILURE);
       case 'f':
-        break;
+
       case 'h':
-        break;
+
       case 'l':
-        break;
+
       case 'q':
-        break;
 
     }
   }
