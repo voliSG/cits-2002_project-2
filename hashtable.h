34d273593e82ab44564ca53c4d7986c54339cf32
agniG <88413285+agniG@users.noreply.github.com>
2021-10-16 10:10:59 +0800

    Added new libraries to the hashtable.h file, and created the uintHash.c file, i have included the hastable.h file in it

--- hashtable.h
+++ hashtable.h
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <string.h>
 // DECLARE FUNCTIONS THAT ARE DEFINED IN hashtable.c
---
+++ uintHash.c
@@ -0,0 +1,12 @@
+#include "hashtable.h"
+
+uint32_t hash_string(char *string)
+{
+    uint32_t hash = 0;
+
+    while(*string != '\0') {
+        hash = hash*33 + *string;
+        ++string;
+    }
+    return hash;
+}
