530f28d47bc9432cd3e8c5a918788ebf76518479
agniG <88413285+agniG@users.noreply.github.com>
2021-10-19 23:49:56 +0800

    getting segmentation error ;~;

--- l_flag.c
+++ l_flag.c
@@ -1,9 +1,21 @@
 #include "duplicates.h"
 
-void l_output(){
-
+void l_output(FILE_DATA *dupfile){
+  for(int i = 0; i < dupfile -> num_files; i++){
+    printf("%s", dupfile -> pathname[i]);
+    printf("  ");
+  }
+  printf("\n");
 }
 
-void l_flag(char **dup_array){
+void l_flag(HASHTABLE *files, char **dup_array){
   printf("in l_flag atm\n");
+  FILE_DATA *dup_file;
+
+  for(int i = 0; i < num_dup; i++){
+    char *dup_hash = dup_array[i];
+    dup_file = hashtable_get(files, dup_hash);
+    l_output(dup_file);
+  }
+
 }
