29dbf49c5df9003aa373a60a43008a6607810572
Sean <2003seangarcia@gmail.com>
2021-10-22 12:12:28 +0800

    comments duplicates.h

--- duplicates.h
+++ duplicates.h
@@ -8,23 +8,26 @@
 
 #include "hashtable.h"
 
-//declare variables for statistics
+// declare global variables for statistics
 extern int      total_files;
 extern int      total_size;
 extern int      min_files;
 extern int      min_size;
 
-//
+// declare global variables for duplicate files
 extern int      num_dup;
 extern char     **hash_array;
 
-// declare function that reads directory
+// declare extern function that reads directory
 extern HASHTABLE    *hash_dir(HASHTABLE *files, char *dirname, bool show_hidden);
 
+
+// declare extern function for program outputs
 extern bool         hf_flags(HASHTABLE *files, char *hash_key, char *ignore);
 extern void         l_flag(HASHTABLE *files, char **dup_array);
 
 // declare SHA2 hash used for files
 extern	char        *strSHA2(char *filename);
 
+// declare strdup function from string.h
 extern  char        *strdup(const char *s);
